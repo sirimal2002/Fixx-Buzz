47a0bb126e9a8cd7b5a47f4ad8f3abbdbf825923
Sirima Lowanitkiattikul <91529364+sirimal2002@users.noreply.github.com>
2022-10-24 13:06:48 +0700

    Add files via upload
    fizzbuzz

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-# 🚨 Don't change the code below 👇
-student_heights = input("Input a list of student heights ").split()
-for n in range(0, len(student_heights)):
-  student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
+#Write your code below this row 👇
 
-
-#Write your code below this row 
-total_h = 0
-for height in student_heights:
-  total_h += height
-print(total_h)
-
-number_of_student = 0
-for number in student_heights:
-  number_of_student += 1
-print(number_of_student)
-
-average = round(total_h/number_of_student)
-print(f"The average is {average}")
-
+for number in range(1,101):
+  if number%5 == 0 and number%3 == 0:
+    print("FizzBuzz")
+  elif number%5 == 0:
+    print("Buzz")
+  elif number%3 == 0:
+    print("Fizz")
+  else :
+    print(number)
